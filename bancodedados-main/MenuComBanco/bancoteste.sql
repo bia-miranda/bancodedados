c1840a228d53c5413e2ba4431e74a965a028c463
bia miranda <112702152+bia-miranda@users.noreply.github.com>
2023-05-15 21:20:25 -0300

    Add files via upload

---
+++ bancodedados-main/MenuComBanco/bancoteste.sql
@@ -0,0 +1,11 @@
+create database bd_teste;
+
+use bd_teste;
+
+create table tb_teste(
+codi int auto_increment primary key,
+nome_t varchar(40),
+senha_t varchar(6),
+sexo_t varchar(1)
+);
+ select * from tb_teste;
